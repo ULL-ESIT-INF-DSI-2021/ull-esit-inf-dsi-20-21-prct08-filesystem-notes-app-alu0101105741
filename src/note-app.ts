d694cd5a878f8e4f89c3b5158a17c62738e4f08d
alu0101105741 <alu0101105741@ull.edu.es>
2021-04-23 14:18:16 +0100

    add hecho, falta el resto

---
+++ src/note-app.ts
@@ -0,0 +1,106 @@
+import * as fs from 'fs';
+import * as chalk from 'chalk';
+import * as yargs from 'yargs';
+import {Note} from './note';
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    user: {
+      describe: 'Note user',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+    color: {
+      describe: 'Note color',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+      if (typeof argv.user === 'string') {
+        if (typeof argv.body === 'string') {
+          if (typeof argv.color === 'string') {
+            // const title = argv.title.toLowerCase().replace(/[^A-Za-z0-9]+(.)/g, (LowLine, chr) => chr.toUpperCase());
+            const path: string = './' + argv.user;
+            const notePath: string = path + '/' + argv.title + '.json';
+            if (fs.existsSync(path)) {
+              if (fs.existsSync(notePath)) {
+                console.log(chalk.red('There is a note with the same title'));
+              } else {
+                fs.appendFileSync(notePath, new Note(argv.user, argv.title, argv.body, argv.color).write());
+                console.log(chalk.green('The note was created succesfully'));
+              }
+            } else {
+              fs.mkdirSync(path);
+              fs.appendFileSync(notePath, new Note(argv.user, argv.title, argv.body, argv.color).write());
+              console.log(chalk.green('The note was created succesfully'));
+            }
+          }
+        }
+      }
+    }
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'Add a new note',
+  builder: {
+    user: {
+      describe: 'Note user',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+    }
+  },
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Add a new note',
+  builder: {
+    user: {
+      describe: 'Note user',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+    }
+  },
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Add a new note',
+  builder: {
+    user: {
+      describe: 'Note user',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.title === 'string') {
+    }
+  },
+});
+
+yargs.parse();
---
+++ src/note.ts
@@ -0,0 +1,37 @@
+/**
+ * asd
+ */
+export class Note {
+  protected user: string;
+  protected title: string;
+  protected body: string;
+  protected color: string;
+
+  /**
+   * asd
+   * @param {string} user asd
+   * @param {string} title asd
+   * @param {string} body asd
+   * @param {string} color asd
+   */
+  constructor(user: string, title: string, body: string, color: string) {
+    this.user = user;
+    this.title = title;
+    this.body = body;
+    this.color = color;
+  }
+
+  /**
+   * asd
+   * @returns {string} asd
+   */
+  write(): string {
+    let json: string = '{\n';
+    json = json + '"user": "' + this.user + '",\n';
+    json = json + '"title": "' + this.title + '",\n';
+    json = json + '"body": "' + this.body + '",\n';
+    json = json + '"color": "' + this.color + '"\n';
+    json = json + '}\n';
+    return json;
+  }
+}
